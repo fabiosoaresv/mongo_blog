a9c2453a851a3992834cfc97465ca3256beda64f
Fabio Soares <soaresventurellifabio@gmail.com>
2023-10-23 16:52:36 -0300

    fix mongodb connection

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module MongoBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    require 'mongoid'
+    Mongoid.load!('config/mongoid.yml')
 
     # Configuration for the application, engines, and railties goes here.
     #
